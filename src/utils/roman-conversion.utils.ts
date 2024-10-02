f59f630851c11a4528f316c30f355aa7423861f5
matias <mmaattuumza@gmail.com>
2024-10-02 14:26:32 -0300

    test-logica

--- src/utils/roman-conversion.utils.ts
+++ src/utils/roman-conversion.utils.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { isRomanNumberValid } from "./roman-nums-validation.utils";
 
 /**
@@ -10,8 +11,69 @@ import { isRomanNumberValid } from "./roman-nums-validation.utils";
  * @param romanNumber 
  * @returns integer value of roman number or 0 if its invalid
  */
+
+//  I: 1,
+//  V: 5,
+//  X: 10,
+//  L: 50,
+//  C: 100,
+//  D: 500,
+//  M: 1000,
 export const romanToInt = (romanNumber: string): number => {
   let sum = 0;
+  const romanNumberArray = romanNumber.split("")
+
+  const romanNumberMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  //MCMXCIX 
+
+
+  for(let i=0; i<romanNumberArray.length; i++){
+    const currentNumber=romanNumberMap[romanNumberArray[i]]
+
+    const nextNumber= romanNumberMap[romanNumberArray[i+1]]
+    // console.log(romanNumberMap[romanNumberArray[i]])
+    if(nextNumber && nextNumber>currentNumber){
+      sum=sum-currentNumber
+    }else{
+      sum = sum + romanNumberMap[currentNumber]
+    }
+    
+  }
+  // romanNumberArray.forEach((el, index)=>{
+     
+  //    if(el==="I" ){
+  //      sum=sum+1
+  //    }
+  //    if(el==="V"){
+  //      sum=sum+5
+  //    }
+  //    if(el==="X"){
+  //      sum=sum+10
+  //    }
+  //    if(el==="L"){
+  //     sum=sum+50
+  //   }
+  //   if(el==="C"){
+  //     sum=sum+100
+  //   }
+  //   if(el==="D"){
+  //     sum=sum+500
+  //   }
+  //   if(el==="M"){
+  //     sum=sum+1000
+  //   }
+  // })
+  switch (romanNumber){
+    
+  }
   if (!isRomanNumberValid(romanNumber)) {
     return sum;
   }
